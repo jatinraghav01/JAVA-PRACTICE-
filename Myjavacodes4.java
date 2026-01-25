fde9f7fd7c6b225f9ec119e4976c896f23ea4665
jatinraghav01 <jatinrag562t@gmail.com>
2026-01-25 23:51:27 +0530

    Add files via upload

---
+++ Myjavacodes4.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Myjavacodes4{
+    public int addLastDigits(int input1, int input2) {
+        return (Math.abs(input1) % 10) + (Math.abs(input2) % 10);
+}
+
+}
