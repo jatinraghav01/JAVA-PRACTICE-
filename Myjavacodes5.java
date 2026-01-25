88329436230aae17b9594ebecc5c1add9bee9ea7
jatinraghav01 <jatinrag562t@gmail.com>
2026-01-25 23:56:38 +0530

    Add files via upload

---
+++ Myjavacodes5.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Myjavacodes5{
+    public int isMultiple(int input1, int input2) {
+        if (input1 == 0 || input2 == 0){
+            return 3;
+        }
+        if (input1 % input2 == 0){
+            return 2;
+        }
+		return 1;
+}
+
+}
