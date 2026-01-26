b1acaef780a79692b96eae9be00fd763cfaa2f64
jatinraghav01 <jatinrag562t@gmail.com>
2026-01-26 16:34:15 +0530

    Add files via upload

---
+++ Myjavacodes10.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Myjavacodes10{
+    public int nFactorial(int input1) {
+    int fact = 1;
+    for (int i = 1; i <= input1; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+
+}
